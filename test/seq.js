1f952b41293e0fddd6e45226554dd7e5c0274abd
Marcus Stade <marcus@stade.se>
2014-03-16 03:27:49 +0100

    Improved coverage of `seq`
    Commented out the generator stuff for now. I'll put it back in
    when I can figure out how to do tests compatible with both ES5
    and ES6. If I can't, I'll ditch it because lol.

--- test/seq.js
+++ test/seq.js
@@ -1,35 +1,57 @@
 const expect = require('chai').expect
+    , each   = require('../lib/each')
     , type   = require('../lib/type')
     , seq    = require('../lib/seq')
 
 describe('seq', function() {
-  describe('when given a falsy value', function() {
+  describe('when given an empty sequence or null value', function() {
     it('should return null', function() {
-      [0, null, undefined, false, ""].forEach(function(falsy) {
-        expect(seq(falsy)).to.equal(null)
+      [null, undefined, [], "", {}].forEach(function(empty) {
+        expect(seq(empty)).to.equal(null)
       })
     })
   })
 
-  const tests =
-        [ [ [1, 2, 3, 4, 5]
-          , [1, 2, 3, 4, 5] 
-          ]
-        , [   { foo: 1,     bar: true,     baz: 'wibble' }
-          , [ { foo: 1 }, { bar: true }, { baz: 'wibble' } ]
-          ]
-        , [  'H'+'e'+'l'+'l'+'o'
-          , ['H','e','l','l','o']
-          ]
-        ]
+  each(
+    [ [ [1, 2, 3, 4, 5]
+      , [1, 2, 3, 4, 5] 
+      ]
+    , [   { foo: 1,     bar: true,     baz: 'wibble' }
+      , [ { foo: 1 }, { bar: true }, { baz: 'wibble' } ]
+      ]
+    , [  'H'+'e'+'l'+'l'+'o'
+      , ['H','e','l','l','o']
+      ]
+    ], function(t) {
+      describe('when given the ' + type(t[0]) + ': ' + t[0], function() {
+        const s = seq(t[0])
 
-  tests.forEach(function(t) {
-    describe('when given the ' + type(t[0]) + ': ' + t[0], function() {
-      const s = seq(t[0])
-
-      it('should return a sequence', function() {
-        expect(s).to.eql(t[1])
+        it('should return a sequence', function() {
+          expect(s).to.eql(t[1])
+        })
       })
+    }
+  )
+
+  describe('when calling `type` on a sequence', function() {
+    it('should return `seq` by calling `typeOf`', function() {
+      const s = seq([1, 2, 3])
+
+      expect(type(s)).to.eql('seq')
     })
   })
+
+  each(
+    [ true
+    , false
+    , 1
+    , 0
+    ], function(test) {
+      describe('when given a ' + type(test), function() {
+        it('should throw an error', function() {
+          expect(function() { seq(test) }).to.throw(TypeError)
+        })
+      })
+    }
+  )
 })
