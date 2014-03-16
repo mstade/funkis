03de97082d86128f048496b297eade31496feec8
Marcus Stade <marcus@stade.se>
2014-03-16 03:13:02 +0100

    Improved test coverage of `eq`

--- test/eq.js
+++ test/eq.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect
+    , each   = require('../lib/each')
     , eq     = require('../lib/eq')
 
 describe('eq', function() {
@@ -12,5 +13,19 @@ describe('eq', function() {
       expect(eq(1, -1)).to.not.be.ok
       expect(eq(0, -0)).to.be.ok
     })
+  })
+
+  each(
+    [ [[1, 2, 3], false]
+    , [[1, 1, 1], true ]
+    , [['a', true, 0], false]
+    , [['a', 'a', 'a'], true]
+    , [[{}, {}, {}], false]
+    ], function(test) {
+      describe('when given ' + test[0], function() {
+        it('should return ' + test[1], function() {
+          expect(eq.apply(null, test[0])).to.equal(test[1])
+        })
+      })
   })
 })
