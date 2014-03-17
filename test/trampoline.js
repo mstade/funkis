0a3eb5fe41a468a2873d501266c100b03a302af3
Marcus Stade <marcus@stade.se>
2014-03-17 02:58:10 +0100

    Added `trampoline` function

---
+++ test/trampoline.js
@@ -0,0 +1,46 @@
+const trampoline = require('../lib/trampoline')
+    , expect     = require('chai').expect
+    , thunk      = require('../lib/thunk')
+    , each       = require('../lib/each')
+
+describe('trampoline', function() {
+  describe('when given a non-function `fn` parameter', function() {
+    it('should return the value of `fn`', function() {
+      each(
+        [ 0, 1
+        , [], {}
+        , true, false
+        , '', 'wibble'
+        , null, undefined
+        ]
+        , function(test) {
+          expect(trampoline(test)).to.equal(test)    
+        }
+      )
+    })
+  })
+
+  describe('when given a factorial function', function() {
+    it('should trampoline it!', function() {
+      const facts = []
+      
+      each([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], function(n) {
+        facts.push(factorial(n))
+      })
+
+      expect(facts).to.eql([1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800])
+
+      function factorial(n) {
+        function calc(acc, n) {
+          if (n) {
+            return thunk(calc, acc * n, n - 1)
+          } else {
+            return acc
+          }
+        }
+
+        return trampoline(calc, 1, n)
+      }
+    })
+  })
+})
