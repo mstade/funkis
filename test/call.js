bff5005ffa0b5e875a577a9a803c0f19f2f3a7a6
Marcus Stade <marcus@stade.se>
2014-03-21 23:09:42 +0100

    Added `call` and `apply`
    The tests for these should be extended when more generic
    sequence code is added. Also, it'd be nice to consolidate the
    tests since they are so similar, but the flu says DRY is so
    overrated.

---
+++ test/call.js
@@ -0,0 +1,79 @@
+describe('call', function() {
+  describe('when not given a function', function() {
+    it('should throw a TypeError', function() {
+      each(
+        [ 0
+        , 1
+        , []
+        , {}
+        , true
+        , false
+        , null
+        , undefined
+        ]
+        , function(nonfn) {
+          expect(function() {
+            call(nonfn)
+          }).to.throw(TypeError)
+        }
+      )
+    })
+  })
+
+  describe('when given a function `fn`', function() {
+    describe('and no arguments', function(done) {
+      it('should call the function without arguments', function(done) {      
+        const fn = function() {
+          expect(arguments.length).to.equal(0)
+          done()
+        }
+
+        call(fn)
+      })
+
+      describe('and `fn` is bound', function() {
+        it('should not affect the binding', function(done) {
+          const owner = {}
+
+          const fn = function() {
+            expect(arguments.length).to.equal(0)
+            expect(this).to.equal(owner)
+            done()
+          }
+
+          call(fn.bind(owner))
+        })
+      })
+    })
+
+    describe('and when given arguments', function(done) {
+      it('should call the function with arguments', function(done) {
+        const fn = function() {
+          expect(slice(arguments)).to.eql([1, true, 'wibble'])
+          done()
+        }
+
+        call(fn, 1, true, 'wibble')
+      })
+
+      describe('and `fn` is bound', function() {
+        it('should not affect the binding', function(done) {
+          const owner = {}
+
+          const fn = function() {
+            expect(slice(arguments)).to.eql([1, true, 'wibble'])
+            expect(this).to.equal(owner)
+            done()
+          }
+
+          call(fn.bind(owner), 1, true, 'wibble')
+        })
+      })
+    })
+  })
+})
+
+const expect = require('chai').expect
+    , slice  = require('../lib/slice')
+    , each   = require('../lib/each')
+    , call   = require('../lib/call')
