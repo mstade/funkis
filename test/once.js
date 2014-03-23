383e401ce15d99e06e622eab9ca2151fa86622a8
Marcus Stade <marcus@stade.se>
2014-03-23 01:08:15 +0100

    Added `once` for one off memoization
    This is mostly useful for parameterless getter style functions
    where subsequent return values will be the same. This'll be
    useful in `seq` where `next` yields a new sequence on the
    first call, but returns the same instance on subsequent calls.

---
+++ test/once.js
@@ -0,0 +1,47 @@
+const expect = require('chai').expect
+    , once   = require('../lib/once')
+
+describe('once', function() {
+  describe('when given a function `fn`', function() {
+    const ofn = once(fn)
+
+    it('should return another function named `once`', function() {
+      expect(ofn).to.be.a('function')
+      expect(ofn.name).to.equal('once')
+    })
+
+    var callcount = 0
+      , result
+
+    describe('and when called', function() {
+      it('should call `fn`', function() {
+        expect(callcount).to.equal(0)
+        result = ofn(5)
+        expect(callcount).to.equal(1)
+        expect(result).to.equal(25)
+      })
+    })
+
+    describe('but if called again', function() {
+      it('should not call `fn`, but return the same value as before', function() {
+        expect(ofn(5)).to.equal(25)
+        expect(callcount).to.equal(1)
+      })
+
+      it('should not call `fn`, even if the parameters change', function() {
+        expect(ofn(2)).to.equal(25)
+        expect(callcount).to.equal(1)
+      })
+    })
+
+    function fn(a) {
+      if (callcount > 1) {
+        throw new Error('Should only be called once!')
+      }
+
+      callcount++
+
+      return a * a
+    }
+  })
+})
