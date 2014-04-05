9e17913fca26ad088651effa4c8c9b0a585be096
Marcus Stade <marcus@stade.se>
2014-04-05 16:39:43 +0200

    Added the `constantly` function

---
+++ test/constantly.js
@@ -0,0 +1,39 @@
+const constantly = require('../lib/constantly')
+    , expect     = require('chai').expect
+    , each       = require('../lib/each')
+    , src        = require('../lib/src')
+
+describe('constantly', function() {
+  each(
+    [ 0
+    , 5
+    , true
+    , false
+    , 'wibble'
+    , Function
+    ]
+    , function(val) {
+      describe('when called with the value `' + src(val) + '`', function() {
+        const v = constantly(val)
+
+        it('should return a function called `constantly`', function() {
+          expect(v.name).to.equal('constantly')
+        })
+
+        describe('that when called with no arguments', function() {
+          it('should return the exact same value', function() {
+            expect(v()).to.equal(val)
+          })
+        })
+
+        describe('and when called with some arguments', function() {
+          it('should still return the exact same value', function() {
+            expect(v(1, 2)).to.equal(val)
+            expect(v(true)).to.equal(val)
+            expect(v(null)).to.equal(val)
+          })
+        })
+      })
+    }
+  )
+})
