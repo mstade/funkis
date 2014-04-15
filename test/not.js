3a082ee17322f6afde8c1d7a15278526a35085e2
Marcus Stade <marcus@stade.se>
2014-04-15 14:13:21 +0200

    Added `not` for logical testing (see [comment][1])
    This will return `true` for values of `false`, `null`, and
    `undefined`. Anything else will return `false`.
    This obviously goes against JS truthy/falsy behavior, but I
    reckon if that's what you'd like just use `!!` or `!`
    respectively; or implement `truthy`/`falsy`.
    
    [1]: https://github.com/mstade/funkis/pull/7#issuecomment-40466894

---
+++ test/not.js
@@ -0,0 +1,35 @@
+const expect = require('chai').expect
+    , each   = require('../lib/each')
+    , src    = require('../lib/src')
+    , not    = require('../lib/not')
+
+describe('not', function() {
+  each(
+    [ null
+    , false
+    , undefined
+    ]
+    , function(x) {
+      describe('when given `'+src(x)+'`', function() {
+        it('should return true', function() {
+          expect(not(x)).to.be.true
+        })
+      })
+    }
+  )
+
+  each(
+    [ 0, 1, -1
+    , true
+    , {}, { foo: 'moo' }
+    , [], [,,], [1,2,3]
+    ]
+    , function(x) {
+      describe('when given `'+src(x)+'`', function() {
+        it('should return false', function() {
+          expect(not(x)).to.be.false
+        })
+      })
+    }
+  )
+})
