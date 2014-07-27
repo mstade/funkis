d6f7842fcad3688e38c9b3ede1341648cfdf7c09
Marcus Stade <marcus@stade.se>
2014-07-28 00:53:46 +0100

    Fix `get` tests

--- test/get.js
+++ test/get.js
@@ -7,31 +7,33 @@ describe('`get`', function() {
   describe('when given two parameters: `map` and `key`;', function() {
     describe('and when `map` has `key`', function() {
       it('should return its value', function () {
-	const map = { foo : 'wibble' }
-	expect(get(map, 'foo')).to.equal('wibble')
+        const map = { foo : 'wibble' }
+        expect(get(map, 'foo')).to.equal('wibble')
       })
     })
 
     describe('and when the value of `key` is logically false', function() {
       it('should still return the value of `key`', function() {
-	each([null, undefined, false], function(x) {
-	  const map = { foo: x }
-	  expect(get(map, 'foo')).to.equal(x)
-	})
+        each([null, undefined, false], function(x) {
+          const map = { foo: x }
+          expect(get(map, 'foo')).to.equal(x)
+        })
       })
     })
 
     describe('but when `map` does not have `key`', function() {
       it('should return `undefined`', function () {
-	const map = { foo : 'wibble' }
-	expect(get(map, 'bar')).to.equal(undefined)
+        const map = { foo : 'wibble' }
+        expect(get(map, 'bar')).to.equal(undefined)
       })
     })
 
     describe('or when `map` is logically false', function() {
-      it('should return `undefined`', partial(each, [null, false, undefined], function (nil) {
-	expect(get(nil, 'whatever')).to.equal(undefined)
-      }))
+      it('should return `undefined`', function() {
+        each([null, false, undefined], function(nil) {
+          expect(get(nil, 'whatever')).to.equal(undefined)
+        })
+      })
     })
   })
 
@@ -40,31 +42,33 @@ describe('`get`', function() {
 
     describe('and when `map` has `key`', function() {
       it('should return its value', function () {
-	const map = { foo : 'wibble' }
-	expect(get(map, 'foo', nope)).to.equal('wibble')
+        const map = { foo : 'wibble' }
+        expect(get(map, 'foo', nope)).to.equal('wibble')
       })
     })
 
     describe('and when the value of `key` is logically false', function() {
       it('should still return the value of `key`', function() {
-	each([null, undefined, false], function(x) {
-	  const map = { foo: x }
-	  expect(get(map, 'foo', nope)).to.equal(x)
-	})
+        each([null, undefined, false], function(x) {
+          const map = { foo: x }
+          expect(get(map, 'foo', nope)).to.equal(x)
+        })
       })
     })
 
     describe('but when `map` does not have `key`', function() {
       it('should return `notFound`', function () {
-	const map  = { foo : 'wibble' }
-	expect(get(map, 'bar', nope)).to.equal(nope)
+        const map  = { foo : 'wibble' }
+        expect(get(map, 'bar', nope)).to.equal(nope)
       })
     })
 
     describe('or when `map` is logically false', function() {
-      it('should return `notFound`', partial(each, [null, false, undefined], function (nil) {
-	expect(get(nil, 'whatever', nope)).to.equal(nope)
-      }))
+      it('should return `notFound`', function() {
+        each([null, false, undefined], function(nil) {
+          expect(get(nil, 'whatever', nope)).to.equal(nope)
+        })
+      })
     })
   })
 })
