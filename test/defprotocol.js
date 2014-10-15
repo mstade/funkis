8fee34e47e6d8f8d38b41782ebde70f60978221a
Marcus Stade <marcus@stade.se>
2014-10-15 02:12:09 +0100

    Some first tests for `defprotocol`
    Damn gamification makes me want to achieve 100% coverage. I'm so easy..

---
+++ test/defprotocol.js
@@ -0,0 +1,50 @@
+var expect      = require('chai').expect
+  , defprotocol = require('../lib/defprotocol')
+
+describe('defprotocol', function() {
+  describe('when given an object', function() {
+    it('should verify the signature of each key')
+
+    describe('and all signatures are ok', function() {
+      var Seq
+
+      it('should return a protocol', function() {
+	Seq = defprotocol('Seq',
+	  { first : []
+	  , rest  : []
+	  })
+
+	expect(Seq).to.be.a('function')
+	expect(Seq.name).to.equal('Protocol')
+      })
+
+      describe('and when implemented', function() {
+	it('should do what it says on the tin', function() {
+	  var Node = function(data, left, right){
+	    this.data = data
+	    this.left = left
+	    this.right = right
+	  }
+
+	  Seq(Node,
+	    { first : function() { return this.data }
+	    , rest  : function() { return [this.left, this.right] }
+	    })
+
+	  var one = new Node(1)
+	    , two = new Node(2)
+	    , tre = new Node(3)
+
+	  one.left   = tre
+	  one.right  = two
+	  two.left   = one
+	  two.right  = tre
+	  tre.left   = two
+	  tre.right  = one
+
+	  expect(Seq(one).first()).to.equal(1)
+	})
+      })
+    })
+  })
+})
