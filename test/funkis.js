bd56585f5519715ebb7b1223d01ec330182cb67f
Marcus Stade <marcus@stade.se>
2014-11-02 23:03:46 +0000

    Made index module test more informative on failure
    Added expect.js; should replace chai over time.

--- test/funkis.js
+++ test/funkis.js
@@ -2,14 +2,14 @@ var fs     = require('fs')
   , path   = require('path')
   , each   = require('../lib/each')
   , funkis = require('../')
-  , expect = require('chai').expect
+  , expect = require('expect.js')
 
 describe('funkis', function() {
   it('should export every function in lib/', function() {
-    var names = fs.readdirSync(path.join(__dirname, '../lib')).map(function(file) {
-      return path.basename(file, '.js')
-    })
-
-    expect(funkis).to.have.keys(names)
+    var names = fs.readdirSync(path.join(__dirname, '../lib'))
+      .map(function(file) {
+        var module = path.basename(file, '.js')
+        expect(funkis).to.have.key(module)
+      })
   })
 })
