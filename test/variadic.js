fa7b133d3014b2a206523f087163ef3d131f2c23
Marcus Stade <marcus@stade.se>
2015-01-31 14:20:33 +0000

    Fix variadic to always set fixed arguments
    If a variadic function is called with fewer arguments than specified by
    `fn`, then the arguments should still be filled, and rest should be empty.

--- test/variadic.js
+++ test/variadic.js
@@ -5,6 +5,7 @@ var variadic = require('../lib/variadic')
   , slice    = require('../lib/slice')
   , each     = require('../lib/each')
   , src      = require('../lib/src')
+  , is       = require('../lib/is')
 
 describe('`variadic`', function() {
   describe('given an argument `fn`', function() {
@@ -32,6 +33,25 @@ describe('`variadic`', function() {
         })
       })
 
+      describe('of arity 1', function() {
+
+        it('should return a 1-arity function', function() {
+          var fn = variadic(function(rest) {})
+          expect(fn).to.have.length(1)
+        })
+
+        describe('when called without arguments', function() {
+          it('should set `rest` to an empty array', function(done) {
+            var fn = variadic(function(rest) {
+              is(Array, rest) && expect(rest).to.have.length(0)
+              done()
+            })
+
+            fn()
+          })
+        })
+      })
+
       each(range(1, 28), function(n) {
         describe('of arity '+n, function() {
           var sig = slice('abcdefghijklmnopqrstuvwxyz', 0, n - 1).concat('rest')
@@ -49,7 +69,7 @@ describe('`variadic`', function() {
 
               while (args.length) {
                 var fn = Function.apply(Function, sig.concat(
-                  [ 'this.expect(arguments).to.have.length('+(args.length + 1)+')'
+                  [ 'this.expect(arguments).to.have.length('+sig.length+')'
                   , 'this.expect(rest).to.be.empty'
                   ].join('\n')
                 ))
